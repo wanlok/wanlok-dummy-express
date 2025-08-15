12aafb883004398f3ad8d7a187016a7d57b0e7b8
Robert Wan <wantunglok@gmail.com>
2025-08-16 09:26:17 +1000

    update pdf page logic

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,20 +1,35 @@
 import { Request, Response } from "express";
-import puppeteer, { PDFOptions } from "puppeteer";
+import puppeteer, { Browser, PDFOptions } from "puppeteer";
 
-const options: PDFOptions = {
+const browserOptions = {
+  executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  headless: true
+};
+
+const pdfOptions: PDFOptions = {
   format: "A4",
   printBackground: true
   // margin: { top: "16px", bottom: "16px" }
 };
 
+const getPage = async (browser: Browser, url: string) => {
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  await page.waitForFunction(() => {
+    const element: HTMLElement | null = document.querySelector("#root");
+    return element && element.innerText.trim().length > 0;
+  });
+  return page;
+};
+
 export const pdf = async (req: Request, res: Response) => {
   const url = req.query.url as string;
+  console.log(url);
   if (url) {
     try {
-      const browser = await puppeteer.launch({});
-      const page = await browser.newPage();
-      await page.goto(url, { waitUntil: "networkidle0" });
-      const pdf = await page.pdf(options);
+      const browser = await puppeteer.launch(browserOptions);
+      const page = await getPage(browser, url);
+      const pdf = await page.pdf(pdfOptions);
       await browser.close();
       res.set("Content-Type", "application/pdf");
       res.send(pdf);
