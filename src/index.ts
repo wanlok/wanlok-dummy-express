06094a8519bf0a4c2e3564fd709f8fbb159add0c
Robert Wan <wantunglok@gmail.com>
2025-08-16 09:53:49 +1000

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-
 import { pdf } from "./pdf";
 import { barChart1 } from "./barChart1";
 
@@ -8,9 +7,9 @@ const port = 3000;
 
 app.use(express.json());
 
-app.post("/bar-chart-1", barChart1);
-
 app.get("/pdf", pdf);
+
+app.post("/bar-chart-1", barChart1);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
