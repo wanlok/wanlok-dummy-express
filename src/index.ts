93833f37cda25ca4273cdb7a66c5f7a312d24c7f
Robert Wan <wantunglok@gmail.com>
2025-08-17 22:15:58 +1000

    update server config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import express from "express";
+import cors from "cors";
+import fs from "fs";
+import https from "https";
 import { pdf } from "./pdf";
 import { barChart1 } from "./barChart1";
 import { upload, uploadParams } from "./uploadHandler";
 import { filePath, fileUploadDirectory, port } from "./config";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "https://wanlok.github.io"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+  })
+);
+
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.crt")
+};
+
 app.use(express.json());
 
 app.use(filePath, express.static(fileUploadDirectory));
@@ -14,6 +31,6 @@ app.get("/pdf", pdf);
 app.post("/bar-chart-1", barChart1);
 app.post(`/upload`, uploadParams, upload);
 
-app.listen(port, () => {
+https.createServer(options, app).listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
